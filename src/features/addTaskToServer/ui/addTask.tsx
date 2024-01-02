3402700eb27c3396ffe3ab5ba13b66b8a2f5b9da
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-02 23:53:59 +0600

    Update length check

--- src/features/addTaskToServer/ui/addTask.tsx
+++ src/features/addTaskToServer/ui/addTask.tsx
@@ -1,31 +1,31 @@
 import { useEffect, useRef, useState } from "react"
 
 const AddTask = () => {
-  useEffect(()=>{
-  },[])
-  
-    function CheckLength(){
-      if(description.length >= 60){
-        setDescriptionCheck(true);
-
-      }
-    }
-
-
     const [task, setTask] = useState('');
     const [click, setClick] = useState(false);
     const [description, setDescription] = useState('');
     const [descriptionCheck, setDescriptionCheck] = useState(false);
-    CheckLength();
-    console.log();
-    //console.log(description)
+
+    useEffect(()=>{
+      
+    },[click])
+
+    function descriptionHandler(e:any){
+      setDescription(e.target.value)
+      if(description.length >= 60){
+        setDescriptionCheck(true);
+      }
+      else{
+        setDescriptionCheck(false)
+      }
+    }
   return (
     <div>
       <input type="text" placeholder="Add Title" className="border-2 border-black rounded-lg m-2" value={task} onChange={(e)=> setTask(e.target.value)}/>
       {click ? 
         <div>
-            <input type="text" placeholder="Write description" className={`border-2 border-black rounded-lg m-2 ${descriptionCheck ? 'text-red': ''}`} value={description} onChange={(e)=> setDescription(e.target.value)}/>
-            <button type="button" className="p-4 bg-red-600 rounded-lg m-2">AddTask</button>
+            <input type="text" placeholder="Write description" className={`border-2 border-black rounded-lg m-2 ${descriptionCheck ? 'text-red-600': 'text-black-600'}`} value={description} onChange={(e)=>descriptionHandler(e)}/>
+            <button type="button" className="p-4 bg-red-600 rounded-lg m-2" onClick={()=>{setClick(!click)}}>AddTask</button>
         </div> 
       : <button type="button" className="p-4 bg-red-600 rounded-lg m-2" onClick={()=> setClick(!click)}>Click Me!</button>}
 
