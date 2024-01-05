e531c057bd04e31237353a0464623cebc8664ee7
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-05 21:45:48 +0600

    Change third theme

--- src/features/ChangeTheme/ui/SwitchThemeButton.tsx
+++ src/features/ChangeTheme/ui/SwitchThemeButton.tsx
@@ -22,10 +22,10 @@ const SwitchThemeButton = () => {
         <button className='px-4 py-2 bg-lime-600 ml-2 mt-4 rounded-lg hover:bg-slate-500' type='button' onClick={()=> dispatch(ChangeTheme('third'))}>Third Theme</button>
     </div> 
     : Theme.ThemeName === 'third' ?
-    <div>
-        <button className='px-4 py-2 bg-teal-500 ml-4 rounded-2xl hover:bg-slate-500' type='button' onClick={()=> dispatch(ChangeTheme('default'))}>default Theme</button>
-        <button className='px-4 py-2 bg-teal-500 ml-4 rounded-2xl hover:bg-slate-500' type='button' onClick={()=> dispatch(ChangeTheme('second'))}>Second Theme</button>
-        <button className='px-4 py-2 bg-teal-500 ml-4 rounded-2xl hover:bg-slate-500' type='button' onClick={()=> dispatch(ChangeTheme('third'))}>Third Theme</button>
+    <div className='flex'>
+        <button className='px-4 py-2 bg-orange-700 ml-4 rounded-2xl hover:bg-slate-500' type='button' onClick={()=> dispatch(ChangeTheme('default'))}>default Theme</button>
+        <button className='px-4 py-2 bg-orange-700 ml-4 rounded-2xl hover:bg-slate-500' type='button' onClick={()=> dispatch(ChangeTheme('second'))}>Second Theme</button>
+        <button className='px-4 py-2 bg-orange-700 ml-4 rounded-2xl hover:bg-slate-500' type='button' onClick={()=> dispatch(ChangeTheme('third'))}>Third Theme</button>
     </div>
     :null}
     </div>
